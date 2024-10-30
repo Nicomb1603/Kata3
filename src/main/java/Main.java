4f25a44675ac2c5d687bf6e366129bf103e151b0
Nicolás Marcelo Batista <nicolasmb1603@gmail.com>
2024-10-30 09:37:40 +0000

    1.0.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,11 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -11,7 +19,43 @@ public class Main {
         Map<Title.TitleType, Integer> histogram = titleTypeHistogram.createHistogram(loadFile("./title.basics.tsv"));
         titleTypeHistogram.printHistogramValues(histogram);
 
+        HistogramaEjemplo example = new HistogramaEjemplo(histogram);
+        example.setSize(800, 600);
+        example.setLocationRelativeTo(null);
+        example.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        example.setVisible(true);
 
+    }
+
+    public static class HistogramaEjemplo extends JFrame {
+
+        public HistogramaEjemplo(Map<Title.TitleType, Integer> histogram) {
+            CategoryDataset dataset = createDataset(histogram);
+
+            JFreeChart chart = ChartFactory.createBarChart(
+                    "Histograma",
+                    "Valores",
+                    "Frecuencia",
+                    dataset,
+                    PlotOrientation.VERTICAL,
+                    false,
+                    true,
+                    false
+            );
+
+            ChartPanel chartPanel = new ChartPanel(chart);
+            chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
+            setContentPane(chartPanel);
+        }
+
+    }
+
+    private static CategoryDataset createDataset(Map<Title.TitleType, Integer> histogram) {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        for(Title.TitleType titleType : histogram.keySet()) {
+           dataset.addValue(histogram.get(titleType), "Frecuencia", titleType);
+        }
+        return dataset;
     }
 
     private static List<Title> loadFile(String s) throws IOException {
